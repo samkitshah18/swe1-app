25f4ee9b8878b823ac333ec646f93cf9b57dac1e
Samkit Shah <samkitvshah18@gmail.com>
2024-02-29 18:45:58 -0500

    Restructuring

--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1a*o+ept9g%_=-c3@vq1)bv%ki)s=#=sczh3iw@)mde6-)486e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["django-env1.eba-mr4mhuvw.us-west-2.elasticbeanstalk.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "django-env1.eba-mr4mhuvw.us-west-2.elasticbeanstalk.com"]
 
 
 # Application definition
