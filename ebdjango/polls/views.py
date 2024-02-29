e44e422361ffa01696f1dc40bce78b9648251cdc
Samkit Shah <samkitvshah18@gmail.com>
2024-02-29 18:02:45 -0500

    Adding comment

--- ebdjango/polls/views.py
+++ ebdjango/polls/views.py
@@ -27,6 +27,7 @@ def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        #minor change to file
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
